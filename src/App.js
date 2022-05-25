cb96bc8dca6bb0c35baf6fbbc0a5bdbd859e33a2
Saju Mulakkal <sajumulakkal@hotmail.com>
2022-05-25 20:39:04 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { ConnectButton, Modal } from "web3uikit";
 import logo from "./images/sLabs.png";
 import Coin from "./components/Coin";
-import {abouts} from "./about";
+import { abouts } from "./about";
 import { useMoralisWeb3Api, useMoralis } from "react-moralis";
 
 const App = () => {
@@ -12,54 +12,56 @@ const App = () => {
   const [link, setLink] = useState(60);
   const [modalPrice, setModalPrice] = useState();
   const Web3Api = useMoralisWeb3Api();
-  const {Moralis, isInitialized} = useMoralis();
+  const { Moralis, isInitialized } = useMoralis();
   const [visible, setVisible] = useState(false);
   const [modalToken, setModalToken] = useState();
 
 
-  async function getRatio(tick, setPerc){
 
-    const Votes = Moralis.Object.extend("Votes");
-    const query = new Moralis.Query(Votes);
-    query.equalTo("ticker", tick);
-    query.descending("createdAt");
-    const results = await query.first();
-    let up = Number(results.attributes.up);
-    let down = Number(results.attributes.down);
-    let ratio = Math.round(up/(up+down)*100);
-    setPerc(ratio);
-  }
 
 
   useEffect(() => {
-    if(isInitialized){
-    getRatio("BTC", setBtc);
-    getRatio("ETH", setEth);
-    getRatio("LINK", setLink);
+    async function getRatio(tick, setPerc) {
 
-
-
-    async function createLiveQuery(){
-      let query = new Moralis.Query('Votes');
-      let subscription = await query.subscribe();
-      subscription.on('update', (object) => {
-        
-        if(object.attributes.ticker === "LINK"){
-          getRatio("LINK", setLink);
-        }else if(object.attributes.ticker === "ETH"){
-          getRatio("ETH", setEth);
-        }else if(object.attributes.ticker === "BTC"){
-          getRatio("BTC", setBtc);
-        }
-
-      });
+      const Votes = Moralis.Object.extend("Votes");
+      const query = new Moralis.Query(Votes);
+      query.equalTo("ticker", tick);
+      query.descending("createdAt");
+      const results = await query.first();
+      let up = Number(results.attributes.up);
+      let down = Number(results.attributes.down);
+      let ratio = Math.round(up / (up + down) * 100);
+      setPerc(ratio);
     }
 
+    if (isInitialized) {
+      getRatio("BTC", setBtc);
+      getRatio("ETH", setEth);
+      getRatio("LINK", setLink);
 
-    createLiveQuery();
+
+
+      async function createLiveQuery() {
+        let query = new Moralis.Query('Votes');
+        let subscription = await query.subscribe();
+        subscription.on('update', (object) => {
+
+          if (object.attributes.ticker === "LINK") {
+            getRatio("LINK", setLink);
+          } else if (object.attributes.ticker === "ETH") {
+            getRatio("ETH", setEth);
+          } else if (object.attributes.ticker === "BTC") {
+            getRatio("BTC", setBtc);
+          }
+
+        });
+      }
+
+
+      createLiveQuery();
     }
 
-  }, [isInitialized]);
+  }, [isInitialized, Moralis]);
 
   useEffect(() => {
 
@@ -72,11 +74,11 @@ const App = () => {
       setModalPrice(price.usdPrice.toFixed(2));
     }
 
-    if(modalToken){
-    fetchTokenPrice()
-  }
-    
-  }, [modalToken]);
+    if (modalToken) {
+      fetchTokenPrice()
+    }
+
+  }, [modalToken, Web3Api]);
 
   return (
     <>
@@ -91,23 +93,23 @@ const App = () => {
         Digital Currency are going Up or Down ?
       </div>
       <div className="list">
-        <Coin 
-          perc={btc} 
-          setPerc={setBtc} 
+        <Coin
+          perc={btc}
+          setPerc={setBtc}
           token={"BTC"}
           setModalToken={setModalToken}
           setVisible={setVisible}
         />
-        <Coin 
-          perc={eth} 
-          setPerc={setEth} 
+        <Coin
+          perc={eth}
+          setPerc={setEth}
           token={"ETH"}
           setModalToken={setModalToken}
           setVisible={setVisible}
         />
-        <Coin 
-          perc={link} 
-          setPerc={setLink} 
+        <Coin
+          perc={link}
+          setPerc={setLink}
           token={"LINK"}
           setModalToken={setModalToken}
           setVisible={setVisible}
@@ -126,14 +128,14 @@ const App = () => {
         </div>
 
 
-         <div>
+        <div>
           <span style={{ color: "white" }}>{`About`}</span>
         </div>
         <div>
           {modalToken &&
             abouts[abouts.findIndex((x) => x.token === modalToken)].about}
         </div>
-       
+
       </Modal>
     </>
   );
